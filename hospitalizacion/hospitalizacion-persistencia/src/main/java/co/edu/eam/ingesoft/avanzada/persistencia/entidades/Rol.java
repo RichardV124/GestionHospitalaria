f077fcb758cd184e3c0504e9856b23fbea916391
lenovo <lenovo@192.168.1.16>
2017-03-22 15:14:30 -0500

    Usuario y Rol mapeado :v

---
+++ hospitalizacion/hospitalizacion-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/Rol.java
@@ -0,0 +1,58 @@
+package co.edu.eam.ingesoft.avanzada.persistencia.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_ROL")
+public class Rol implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+
+	
+	public Rol() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	
+	
+}
---
+++ hospitalizacion/hospitalizacion-persistencia/src/main/java/co/edu/eam/ingesoft/avanzada/persistencia/entidades/Usuario.java
@@ -0,0 +1,60 @@
+package co.edu.eam.ingesoft.avanzada.persistencia.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_USUARIO")
+@NamedQueries({
+@NamedQuery(name="Usuario.buscarXUser",query="SELECT u FROM Usuario u WHERE u.user=?1")
+})
+public class Usuario implements Serializable {
+
+	public static final String BUSCAR_POR_USUARIO = "Usuario.buscarXUser";
+	
+	@Id
+	@Column(unique=true,name="usuario")
+	private String user;
+	
+	@Column(name="password")
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name="idrol")
+	private Rol rol;
+	
+	public Usuario() {
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+}
